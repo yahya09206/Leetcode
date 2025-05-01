f6510e2061a12aeaead351195ce13d555af9ff40
yahya09206 <yahyahussein91@gmail.com>
2025-05-01 13:19:38 -0700

    restarting leetcode

--- src/BinarySearch/Classic.java
+++ src/BinarySearch/Classic.java
@@ -11,21 +11,6 @@ public class Classic {
 
     public static int binarySearch(int[] arr, int target) {
 
-        int low = 0;
-        int high = arr.length - 1;
-
-        while(low <= high){
-
-            int mid = low + (high - low ) / 2;
-
-            if(arr[mid] > target){
-                high = mid - 1;
-            } else if(arr[mid] < target){
-                low = mid + 1;
-            } else {
-                return mid;
-            }
-        }
         return -1;
     }
 }
