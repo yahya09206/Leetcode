8fdf3ba6a2829ce3715a8f709ea3c1d88fe8eeaf
Yahya <yahyahussein91@gmail.com>
2025-05-03 02:15:17 -0700

    finished with classic binary search

--- src/BinarySearch/Classic.java
+++ src/BinarySearch/Classic.java
@@ -11,6 +11,23 @@ public class Classic {
 
     public static int binarySearch(int[] arr, int target) {
 
+        // establish starting points
+        int low = 0;
+        int high = arr.length - 1;
+
+        while(low <= high){
+
+            // find middle point and prevent overflow
+            int mid = low + (high - low) / 2;
+
+            if (target < arr[mid]){
+                high = mid - 1;
+            } else if (target > arr[mid]){
+                low = mid + 1;
+            } else {
+                return mid;
+            }
+        }
         return -1;
     }
 }
