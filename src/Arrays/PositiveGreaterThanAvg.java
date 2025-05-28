a870f1898a5bc8a746b01a432bc10c1d8c753596
yahya09206 <yahyahussein91@gmail.com>
2025-05-27 22:01:10 -0700

    stuck on count nums greater than average

---
+++ src/Arrays/PositiveGreaterThanAvg.java
@@ -0,0 +1,22 @@
+package Arrays;
+
+public class PositiveGreaterThanAvg {
+    public static void main(String[] args) {
+
+        int[] nums = {13,4,45,6};
+        System.out.println(numsGreaterThanAvg(nums));
+    }
+
+    public static int numsGreaterThanAvg(int[] arr){
+
+        int count = 0;
+        int sum = 0;
+        for (int i = 0; i <= arr.length - 1; i++){
+            sum += arr[i];
+        }
+        if (arr[i] > (double) sum / (arr.length)){
+            count++;
+        }
+        return count;
+    }
+}
