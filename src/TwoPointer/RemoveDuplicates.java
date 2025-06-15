958e135c8525e89a45ecb2d3514cd245e3f0bdb5
Yahya <yahyahussein91@gmail.com>
2025-06-15 02:46:18 -0700

    removing duplicates from array

---
+++ src/TwoPointer/RemoveDuplicates.java
@@ -0,0 +1,23 @@
+package TwoPointer;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class RemoveDuplicates {
+    public static void main(String[] args) {
+
+        int[] nums = {1,1,2,2,3,4};
+        System.out.println(removeDuplicates(nums));
+    }
+
+    public static int removeDuplicates(int[] arr){
+
+        int i = 0;
+        for (int j = 1; j <= arr.length -1; j++){
+            if (arr[i] != arr[j]){
+                arr[i + 1] = arr[j];
+                i++;
+            }
+        }
+        return i + 1;
+    }
+}
