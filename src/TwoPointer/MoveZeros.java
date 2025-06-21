b05a4e9200c0601f0f2f2cb7ff9fd44612a4ef25
Yahya <yahyahussein91@gmail.com>
2025-06-20 23:27:58 -0700

    iterating thru array

--- src/TwoPointer/MoveZeros.java
+++ src/TwoPointer/MoveZeros.java
@@ -5,7 +5,10 @@ public class MoveZeros {
 
         int[] nums = {1,2,0,3,4,0,4};
     }
-    public static int removeZeros(){
+    public static int removeZeros(int[] arr){
 
+        for (int i = 0; i <= arr.length - 1; i++){
+
+        }
     }
 }
