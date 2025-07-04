8a6da316ae7968461d3076022c715c576ffc05c6
yahya09206 <yahyahussein91@gmail.com>
2025-07-03 17:49:24 -0700

    setting up starter code

---
+++ src/TwoPointer/FirstRun/ReverseArray.java
@@ -0,0 +1,12 @@
+package TwoPointer.FirstRun;
+
+import java.sql.SQLOutput;
+
+public class ReverseArray {
+    public static void main(String[] args) {
+
+        int[] nums = {1,2,3,4,5,6,7,8};
+        System.out.println(reverse(nums));
+    }
+
+}
