ab58108ffeb2764281dc4700bdaeb39ad5343a51
Yahya <yahyahussein91@gmail.com>
2025-07-04 21:43:42 -0700

    need to restart remomving zeros

--- src/TwoPointer/MoveZeros.java
+++ src/TwoPointer/MoveZeros.java
@@ -4,13 +4,6 @@ public class MoveZeros {
     public static void main(String[] args) {
 
         int[] nums = {1,2,0,3,4,0,4};
-        removeZeros.sout;
     }
-    public static int removeZeros(int[] arr){
 
-        for (int i = 0; i <= arr.length - 1; i++){
-
-        }
-        return arr;
-    }
 }
