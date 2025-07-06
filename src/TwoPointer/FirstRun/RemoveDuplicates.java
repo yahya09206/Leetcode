37e12cd524b4c1cac96027362c4078cfad3c096f
Yahya <yahyahussein91@gmail.com>
2025-07-06 03:09:50 -0700

    logic for removing duplicates

--- src/TwoPointer/FirstRun/RemoveDuplicates.java
+++ src/TwoPointer/FirstRun/RemoveDuplicates.java
@@ -9,17 +9,19 @@ public class RemoveDuplicates {
 
     public static int removeDuplicates(int[] arr){
 
-
         // Set left to 0
         int l = 0;
-        // Loop thru array with r pointer
-        for(int r = 1; r < arr.length; r++){
-            // Check if current index nums[r] != nums[r-1]
-            // arr[r] = l
-            // nums[k++] = nums[r]
-            // return l
+        // Loop through array with r pointer
+        for(int r = 1; r <= arr.length - 1; r++){
+            // Check if current index arr[l+1] != arr[r]
+            if (arr[l] != arr[r]){
+                // Assign r pointer to next l pointer
+                arr[l+1] = arr[r];
+                // Increment l pointer
+                l++;
+            }
         }
-
-        return l;
+        // Return l + 1
+        return l + 1;
     }
 }
