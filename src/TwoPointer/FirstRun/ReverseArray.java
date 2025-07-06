225cc8eba3b79a6cbc983b157955d34f39e8d7f1
Yahya <yahyahussein91@gmail.com>
2025-07-06 02:55:33 -0700

    added more checks to make sure logic is working

--- src/TwoPointer/FirstRun/ReverseArray.java
+++ src/TwoPointer/FirstRun/ReverseArray.java
@@ -6,7 +6,7 @@ import java.util.Arrays;
 public class ReverseArray {
     public static void main(String[] args) {
 
-        int[] nums = {1,2,3,4,5,6,7,8};
+        int[] nums = {1,2,3,4,5,6,7,8,9,10};
         System.out.println(Arrays.toString(reverse(nums)));
     }
 
