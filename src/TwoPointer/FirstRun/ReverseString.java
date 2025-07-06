a62259d64adc9a9dda91d0095ffe357eb07bc2ec
Yahya <yahyahussein91@gmail.com>
2025-07-06 03:13:13 -0700

    set up starter code

---
+++ src/TwoPointer/FirstRun/ReverseString.java
@@ -0,0 +1,14 @@
+package TwoPointer.FirstRun;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        String sentence = "reverse this string";
+        System.out.println(reverseString(sentence));
+    }
+
+    public static String[] reverseString(String[] str){
+
+        return str;
+    }
+}
