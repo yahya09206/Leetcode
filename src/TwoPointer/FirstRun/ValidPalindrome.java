18499e3587e9c72f7d6f9fa25ee825353e000a3d
yahya09206 <yahyahussein91@gmail.com>
2025-07-07 18:54:48 -0700

    initializing pointers

--- src/TwoPointer/FirstRun/ValidPalindrome.java
+++ src/TwoPointer/FirstRun/ValidPalindrome.java
@@ -10,12 +10,14 @@ public class ValidPalindrome {
         if (s == null || s.isEmpty()){
             return true;
         }
-//- init start and end pointers
-//- 2 vars to keep track of char index
-//- check if current charAt(i) is a lettorOrNumber for both sides
-//- return false is left char is != to right char
-//- else increase left and decrement right
-//- return true if above = true
+        //- init start and end pointers
+        int start = 0;
+        int end = s.length() - 1;
+        //- 2 vars to keep track of char index
+        //- check if current charAt(i) is a lettorOrNumber for both sides
+        //- return false is left char is != to right char
+        //- else increase left and decrement right
+        //- return true if above = true
 
         return true;
     }
