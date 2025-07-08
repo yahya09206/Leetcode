433b357bccad61111616e4827b73143910a68502
yahya09206 <yahyahussein91@gmail.com>
2025-07-07 18:53:48 -0700

    return true if string is empty or null

---
+++ src/TwoPointer/FirstRun/ValidPalindrome.java
@@ -0,0 +1,22 @@
+package TwoPointer.FirstRun;
+
+public class ValidPalindrome {
+    public static void main(String[] args) {
+
+    }
+    public static boolean isPalindrome(String s){
+
+//        -  return true if string is empty or null
+        if (s == null || s.isEmpty()){
+            return true;
+        }
+//- init start and end pointers
+//- 2 vars to keep track of char index
+//- check if current charAt(i) is a lettorOrNumber for both sides
+//- return false is left char is != to right char
+//- else increase left and decrement right
+//- return true if above = true
+
+        return true;
+    }
+}
