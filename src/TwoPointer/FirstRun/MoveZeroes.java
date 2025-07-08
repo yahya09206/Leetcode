2c8b8ebe2cc0e80cab005afe643c210b4d77aa23
yahya09206 <yahyahussein91@gmail.com>
2025-07-07 18:18:02 -0700

    added pseudocode for moving zeroes

---
+++ src/TwoPointer/FirstRun/MoveZeroes.java
@@ -0,0 +1,23 @@
+package TwoPointer.FirstRun;
+
+public class MoveZeroes {
+    public static void main(String[] args) {
+
+
+    }
+
+    public void moveZeroes(int[] nums){
+
+        // init left pointer
+        int l = 0;
+
+        // iterate through array with right pointer
+        // this loop will increment right pointer
+            // if right pointer != 0
+                // store value of right pointer into temp variable
+                // store value of left pointer into right pointer
+                // store value of temp value into left pointer
+                // increment left pointer
+
+    }
+}
