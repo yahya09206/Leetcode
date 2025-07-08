13be8a8246c2a05f9ca9c86155675115df9cb4f5
yahya09206 <yahyahussein91@gmail.com>
2025-07-07 18:36:02 -0700

    psuedocode for remove element

---
+++ src/TwoPointer/FirstRun/RemoveElement.java
@@ -0,0 +1,16 @@
+package TwoPointer.FirstRun;
+
+public class RemoveElement {
+    public static void main(String[] args) {
+
+    }
+    public int removeElement(int[] nums, int val){
+
+        //initialize left pointer
+
+        //loop through with right pointer
+            // check if nums[r] != val
+                // switch value at nums[l] with value at current index nums[r]
+                // increment left pointer
+    }
+}
