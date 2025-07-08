74a851f4d3df106a677da104ee1b8d3346567e39
yahya09206 <yahyahussein91@gmail.com>
2025-07-07 18:37:35 -0700

    added code for removing an element

--- src/TwoPointer/FirstRun/RemoveElement.java
+++ src/TwoPointer/FirstRun/RemoveElement.java
@@ -7,10 +7,20 @@ public class RemoveElement {
     public int removeElement(int[] nums, int val){
 
         //initialize left pointer
+        int l = 0;
 
         //loop through with right pointer
+        for (int r = 0; r <= nums.length - 1; r++){
             // check if nums[r] != val
+            if (nums[r] != val){
                 // switch value at nums[l] with value at current index nums[r]
+                nums[l] = nums[r];
                 // increment left pointer
+                l++;
+            }
+        }
+
+        // return left pointer
+        return l;
     }
 }
