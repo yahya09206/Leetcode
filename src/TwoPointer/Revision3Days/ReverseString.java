b0d1c913a6f5022ecb22d55cca65b0c6fe648370
yahya09206 <yahyahussein91@gmail.com>
2025-07-11 12:46:36 -0700

    setting up reverse string for revision

---
+++ src/TwoPointer/Revision3Days/ReverseString.java
@@ -0,0 +1,19 @@
+package TwoPointer.Revision3Days;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+    }
+
+    public static void reverseString(char[] str){
+
+        int l = 0;
+        int r = 0;
+
+        while (l < r){
+
+            char temp = str[l];
+
+        }
+    }
+}
