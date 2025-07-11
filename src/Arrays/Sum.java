3f8caa3b29bf2524da85bf3f5617c39ac974a622
yahya09206 <yahyahussein91@gmail.com>
2025-07-10 23:15:17 -0700

    need to start from scratch

--- src/Arrays/Sum.java
+++ src/Arrays/Sum.java
@@ -1,7 +1,7 @@
 package Arrays;
 
 public class Sum {
-    public static int sum(int sum1, int sum2){
-        return sum1 + sum2;
+    public static int sum(int sum1, int sum2, int sum3){
+        return sum1 + sum2 + sum3;
     }
 }
