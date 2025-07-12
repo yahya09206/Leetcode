a58ad9e6ac31104ba263f0d0764990a73c1bf339
yahya09206 <yahyahussein91@gmail.com>
2025-07-11 17:01:19 -0700

    forgot to iterate at end of str

--- src/TwoPointer/Revision3Days/ReverseString.java
+++ src/TwoPointer/Revision3Days/ReverseString.java
@@ -3,14 +3,12 @@ package TwoPointer.Revision3Days;
 public class ReverseString {
     public static void main(String[] args) {
 
-        String str = "remove";
-        System.out.println(reverseString(str));
     }
 
     public static void reverseString(char[] str){
 
         int l = 0;
-        int r = 0;
+        int r = str.length - 1;
 
         while (l < r){
 
