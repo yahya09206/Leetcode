3dd33b45d9e8e70c5f42f50c461c15603591c689
Yahya <yahyahussein91@gmail.com>
2025-07-12 01:15:06 -0700

    starting on fizzbuzz using arrayList

---
+++ src/Arrays/FizzBuzz.java
@@ -0,0 +1,14 @@
+package Arrays;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz {
+    public static void main(String[] args) {
+
+    }
+    public static List<String> fizzBuz(int n){
+
+        List<String> result = new ArrayList<>();
+    }
+}
