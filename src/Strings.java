464a2f97e240698987b2f32bb414a37e986bef6e
Yahya <yahyahussein91@gmail.com>
2025-07-12 04:10:42 -0700

    is anagram completed

---
+++ src/Strings.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class Strings {
+    public static void main(String[] args) {
+
+        String str1 = "anagram";
+        String str2 = "nagaram";
+        System.out.println(isAnagram(str1, str2));
+    }
+
+    public static boolean isAnagram(String s, String t){
+
+        char[] s1 = s.toCharArray();
+        char[] s2 = t.toCharArray();
+
+        Arrays.sort(s1);
+        Arrays.sort(s2);
+
+        return Arrays.equals(s1, s2);
+    }
+}
