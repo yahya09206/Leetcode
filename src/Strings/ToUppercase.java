b4b7ad5ac716bd3e314d496dad0c730e86d4ac87
Yahya <yahyahussein91@gmail.com>
2025-07-19 02:08:45 -0700

    converting string to uppercase

---
+++ src/Strings/ToUppercase.java
@@ -0,0 +1,13 @@
+package Strings;
+
+public class ToUppercase {
+    public static void main(String[] args) {
+
+        String str = "hello";
+        System.out.println(toUppercase(str));
+    }
+    public static String toUppercase(String str){
+
+        return str.toUpperCase();
+    }
+}
