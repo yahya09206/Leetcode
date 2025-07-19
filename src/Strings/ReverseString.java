2085e0e5a38c33d1d90400ed4123a511b45a6364
Yahya <yahyahussein91@gmail.com>
2025-07-19 02:11:25 -0700

    reversing string

---
+++ src/Strings/ReverseString.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class ReverseString {
+    public static void main(String[] args) {
+
+        String str = "hello";
+        System.out.println(reverseString(str));
+    }
+
+    public static String reverseString(String s){
+
+        String reversed = "";
+        for (int i = s.length() - 1; i >= 0; i--){
+            reversed += s.charAt(i);
+        }
+        return reversed;
+    }
+}
